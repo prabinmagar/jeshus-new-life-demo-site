023608c23154d65cbd6aa871daa0994b50329f12
prabin.thapamagar <prabin_mi@yonefu.info>
2023-11-24 15:46:21 +0545

    even dialog upage updated

--- src/components/common/EventDialog.jsx
+++ src/components/common/EventDialog.jsx
@@ -1,11 +1,7 @@
-import {
-  Button,
-  IconButton,
-  Typography,
-} from "@material-tailwind/react";
+import { Button, IconButton, Typography } from "@material-tailwind/react";
 import PropTypes from "prop-types";
 import staticImages from "../../utils/images";
-import { FaMapMarkerAlt, FaTimes } from "react-icons/fa";
+import { FaCalendarAlt, FaMapMarkerAlt, FaTimes } from "react-icons/fa";
 
 export function EventDialog({ eventDialogOpen, handleEventDialogOpen }) {
   return (
@@ -19,7 +15,7 @@ export function EventDialog({ eventDialogOpen, handleEventDialogOpen }) {
           <div className="justify-between lg:px-8 px-6 py-4">
             <div className="flex items-center gap-3 mb-2 w-full border-b-[1px] border-blue-gray-50">
               <div className="flex w-full justify-between flex-wrap gap-x-4 items-center mb-2">
-                <Typography className="font-medium font-cera text-lg text-dark">
+                <Typography className="font-semibold font-cera text-lg text-dark">
                   2073 Annual Seminar With Congregation
                 </Typography>
                 <IconButton
@@ -35,35 +31,49 @@ export function EventDialog({ eventDialogOpen, handleEventDialogOpen }) {
             <div className="grid md:grid-cols-2 gap-x-6 md:mb-5">
               <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 h-[240px] red-overlay">
                 <img
-                  src={staticImages.notice1}
+                  src={staticImages.notice2}
                   className="w-full h-full object-cover"
                 />
               </div>
-              <div className="pt-2 pb-5 flex flex-col justify-between flex-1">
+              <div className="pt-2 pb-3 flex flex-col justify-between flex-1">
                 <div className="mb-3 flex items-start flex-col">
                   <div className="flex flex-col">
                     <div className="w-full text-2xl leading-8 text-gray-200 font-bold mb-3 mt-3">
-                      Christian Hymns Seminar
-                    </div>
-                    <div className="flex-auto text-gray-400 my-1">
-                      <span className="mr-3">December 35, 2023</span>
-                      <span className="mr-3 border-r border-gray-600  max-h-0"></span>
-                      <span>Friday</span>
+                      Hillsong Hymns Seminar
                     </div>
                   </div>
-                  <div className="flex flex-row items-center mt-3 bg-blue/10 rounded-md py-1.5 px-3">
-                    <div className="inline-flex items-center gap-x-3">
-                      <FaMapMarkerAlt />
-                      <p className="text-sm">House of God, Lalitpur, NP</p>
+                  <div className="flex flex-wrap items-center gap-x-4 gap-y-1 border-b-[1px] border-gray/10 w-full pb-2">
+                    <div className="inline-flex flex-row items-center">
+                      <div className="inline-flex items-center gap-x-2">
+                        <FaMapMarkerAlt className="text-dark" size={13} />
+                        <p className="text-sm text-gray">
+                          House of Worship, Lalitpur
+                        </p>
+                      </div>
+                    </div>
+                    <div className="inline-flex flex-row items-center">
+                      <div className="inline-flex items-center gap-x-2">
+                        <FaCalendarAlt className="text-dark" size={12} />
+                        <p className="text-sm text-gray">
+                          <span className="mr-3">Dec 35, 2023</span>
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <Button
-                    variant="gradient"
-                    size="sm"
-                    className="font-medium capitalize font-cera rounded-full text-sm bg-red mt-4"
+                  <div className="flex items-center flex-wrap gap-x-3 mb-1 mt-5 bg-whitesmoke p-3 w-full rounded border-[1px] border-gray/10">
+                    <span className="w-[42px] h-[40px] green-gradient inline-flex items-center justify-center text-white rounded text-3xl font-semibold">
+                      10
+                    </span>
+                    <span className="font-semibold text-dark text-lg">
+                      Days Remaining
+                    </span>
+                  </div>
+                  <button
+                    className="btn bg-red capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition mt-4"
+                    type="button"
                   >
-                    Register Here !
-                  </Button>
+                    Register Now!
+                  </button>
                 </div>
               </div>
             </div>
--- src/screens/event/EventScreen.jsx
+++ src/screens/event/EventScreen.jsx
@@ -163,12 +163,6 @@ const EventScreen = () => {
                   </div>
                   <div className="flex items-center gap-x-3 mt-3">
                     <button
-                      className="btn bg-red capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
-                      type="button"
-                    >
-                      Register Now!
-                    </button>
-                    <button
                       className="btn bg-blue capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
                       type="button"
                     >
@@ -254,6 +248,260 @@ const EventScreen = () => {
                 <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-dark/20 h-[220px]">
                   <img
                     src={staticImages.notice4}
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="absolute z-10 top-0 rotate-180 left-3 border-t-[20px] border-t-transparent border-l-[36px] border-l-red border-r-[36px] border-r-red border-b-[80px] border-b-red shadow-lg">
+                    <div className="absolute top-5 rotate-180 flex text-center right-1/2 translate-x-1/2 leading-[6px] gap-y-2 font-bold uppercase flex-col">
+                      <span className="text-3xl">29</span>
+                      <span>Dec</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:px-5 p-4 flex flex-col justify-between flex-1">
+                  <div className="mb-3 flex items-start flex-col">
+                    <div className="flex flex-col">
+                      <div className="w-full text-xl xl:leading-8 text-gray-200 font-bold mb-3">
+                        2073 Annual Seminar With Congregation
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap items-center gap-x-4 gap-y-1">
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaMapMarkerAlt className="text-dark" size={13} />
+                          <p className="text-sm text-gray">
+                            House of Worship, Lalitpur
+                          </p>
+                        </div>
+                      </div>
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaCalendarAlt className="text-dark" size={12} />
+                          <p className="text-sm text-gray">
+                            <span className="mr-3">Dec 35, 2023</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-x-2 mb-1 bg-green/5 py-2 px-2.5 border-r-4 border-green rounded">
+                    <span className="w-[26px] h-[24px] green-gradient inline-flex items-center justify-center text-white rounded font-semibold">
+                      10
+                    </span>
+                    <span className="font-semibold text-green">
+                      Days Remaining
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-x-3 mt-3">
+                    <button
+                      className="btn bg-red capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
+                      type="button"
+                    >
+                      Register Now!
+                    </button>
+                    <button
+                      className="btn bg-blue capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition border-[1px]"
+                      type="button"
+                    >
+                      See Details
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="notice-card group relative flex w-full mx-auto flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-normal font-cera cursor-pointer"
+                onClick={onEventDialogOpen}
+                data-aos="fade-up"
+              >
+                <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-dark/20 h-[220px]">
+                  <img
+                    src={staticImages.temp3}
+                    className="w-full h-full object-cover group-hover:scale-110 default-transition"
+                  />
+                  <div className="absolute z-10 top-0 rotate-180 left-3 border-t-[20px] border-t-transparent border-l-[36px] border-l-red border-r-[36px] border-r-red border-b-[80px] border-b-red shadow-lg">
+                    <div className="absolute top-5 rotate-180 flex text-center right-1/2 translate-x-1/2 leading-[6px] gap-y-2 font-bold uppercase flex-col">
+                      <span className="text-3xl">29</span>
+                      <span>Dec</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:px-5 p-4 flex flex-col justify-between flex-1">
+                  <div className="mb-3 flex items-start flex-col">
+                    <div className="flex flex-col">
+                      <div className="w-full text-xl xl:leading-8 text-gray-200 font-bold mb-3">
+                        2073 Annual Seminar With Congregation
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap items-center gap-x-4 gap-y-1">
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaMapMarkerAlt className="text-dark" size={13} />
+                          <p className="text-sm text-gray">
+                            House of Worship, Lalitpur
+                          </p>
+                        </div>
+                      </div>
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaCalendarAlt className="text-dark" size={12} />
+                          <p className="text-sm text-gray">
+                            <span className="mr-3">Dec 35, 2023</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-x-2 mb-1 bg-red/5 py-2 px-2.5 border-r-4 border-red rounded">
+                    <span className="w-[26px] h-[24px] red-gradient inline-flex items-center justify-center text-white rounded font-semibold">
+                      1
+                    </span>
+                    <span className="font-semibold text-red">
+                      Days Remaining
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-x-3 mt-3">
+                    <button
+                      className="btn bg-blue capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
+                      type="button"
+                    >
+                      See Details
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="notice-card group relative flex w-full mx-auto flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-normal font-cera cursor-pointer"
+                data-aos="fade-up"
+                onClick={onEventDialogOpen}
+              >
+                <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-dark/20 h-[220px]">
+                  <img
+                    src={staticImages.article2}
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="absolute z-10 top-0 rotate-180 left-3 border-t-[20px] border-t-transparent border-l-[36px] border-l-red border-r-[36px] border-r-red border-b-[80px] border-b-red shadow-lg">
+                    <div className="absolute top-5 rotate-180 flex text-center right-1/2 translate-x-1/2 leading-[6px] gap-y-2 font-bold uppercase flex-col">
+                      <span className="text-3xl">29</span>
+                      <span>Dec</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:px-5 p-4 flex flex-col justify-between flex-1">
+                  <div className="mb-3 flex items-start flex-col">
+                    <div className="flex flex-col">
+                      <div className="w-full text-xl xl:leading-8 text-gray-200 font-bold mb-3">
+                        2073 Annual Seminar With Congregation
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap items-center gap-x-4 gap-y-1">
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaMapMarkerAlt className="text-dark" size={13} />
+                          <p className="text-sm text-gray">
+                            House of Worship, Lalitpur
+                          </p>
+                        </div>
+                      </div>
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaCalendarAlt className="text-dark" size={12} />
+                          <p className="text-sm text-gray">
+                            <span className="mr-3">Dec 35, 2023</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-x-2 mb-1 bg-green/5 py-2 px-2.5 border-r-4 border-green rounded">
+                    <span className="w-[26px] h-[24px] green-gradient inline-flex items-center justify-center text-white rounded font-semibold">
+                      10
+                    </span>
+                    <span className="font-semibold text-green">
+                      Days Remaining
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-x-3 mt-3">
+                    <button
+                      className="btn bg-blue capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
+                      type="button"
+                    >
+                      See Details
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="notice-card group relative flex w-full mx-auto flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-normal font-cera"
+                data-aos="fade-up"
+                onClick={onEventDialogOpen}
+              >
+                <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-dark/20 h-[220px]">
+                  <img
+                    src={staticImages.temp2}
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="absolute z-10 top-0 rotate-180 left-3 border-t-[20px] border-t-transparent border-l-[36px] border-l-blue border-r-[36px] border-r-blue border-b-[80px] border-b-blue shadow-lg">
+                    <div className="absolute top-5 rotate-180 flex text-center right-1/2 translate-x-1/2 leading-[6px] gap-y-2 font-bold uppercase flex-col">
+                      <span className="text-3xl">29</span>
+                      <span>Dec</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:px-5 p-4 flex flex-col justify-between flex-1">
+                  <div className="mb-3 flex items-start flex-col">
+                    <div className="flex flex-col">
+                      <div className="w-full text-xl xl:leading-8 text-gray-200 font-bold mb-3">
+                        2073 Annual Seminar With Congregation
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap items-center gap-x-4 gap-y-1">
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaMapMarkerAlt className="text-dark" size={13} />
+                          <p className="text-sm text-gray">
+                            House of Worship, Lalitpur
+                          </p>
+                        </div>
+                      </div>
+                      <div className="inline-flex flex-row items-center">
+                        <div className="inline-flex items-center gap-x-2">
+                          <FaCalendarAlt className="text-dark" size={12} />
+                          <p className="text-sm text-gray">
+                            <span className="mr-3">Dec 35, 2023</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center flex-wrap gap-x-2 mb-1 bg-green/5 py-2 px-2.5 border-r-4 border-green rounded">
+                    <span className="w-[26px] h-[24px] green-gradient inline-flex items-center justify-center text-white rounded font-semibold">
+                      10
+                    </span>
+                    <span className="font-semibold text-green">
+                      Days Remaining
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-x-3 mt-3">
+                    <button
+                      className="btn bg-blue capitalize py-1.5 px-3 text-center shadow-md rounded focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none default-transition"
+                      type="button"
+                    >
+                      See Details
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="notice-card group relative flex w-full mx-auto flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-normal font-cera"
+                data-aos="fade-up"
+                onClick={onEventDialogOpen}
+              >
+                <div className="relative overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-dark/20 h-[220px]">
+                  <img
+                    src={staticImages.temp1}
                     className="w-full h-full object-cover"
                   />
                   <div className="absolute z-10 top-0 rotate-180 left-3 border-t-[20px] border-t-transparent border-l-[36px] border-l-red border-r-[36px] border-r-red border-b-[80px] border-b-red shadow-lg">
